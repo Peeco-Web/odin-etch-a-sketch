63cd1a52557c51481646e3d82b1715034b381880
amir <amfar77@gmail.com>
2023-05-04 18:39:26 -0400

    add full functionality on mouse hover

--- scripts.js
+++ scripts.js
@@ -4,15 +4,17 @@ const container = document.querySelector('.container');
 // Create the row and box elements that make up the grid
 const row = document.createElement('div');
 const div = document.createElement('div');
-row.setAttribute("class","row")
-div.setAttribute("class","box")
+row.setAttribute("class","row");
+div.setAttribute("class","box");
 
 // Append
 container.append(row);
 row.append(div);
 
 
-// Function that creates n number of boxes per row
+// Function that creates nxn grid
+n = 30;
+
 function makeGrid(n) {
     for (i=1;i<n;i++) {
         row.append(div.cloneNode(true));
@@ -22,26 +24,15 @@ function makeGrid(n) {
     }
 }
 
-
-makeGrid(64)
-
+makeGrid(n);
 
 
+// Function to add color attribute to boxes on hover
+boxes = document.querySelectorAll('.box');
 
-
-//Function that creates n number of rows based on number of boxes
-
-
-
-
-
-
-
-
-
-
-
-// Create a flexbox of 16 horizontal grids
-
-
-// Create a flexbox of 16 rows of the same grid line
+boxes.forEach(box =>{
+    box.addEventListener('mouseover', function handleClick(e){
+        box.setAttribute('style','background-color: black')
+    })
+})
+       
